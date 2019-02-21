8ca93b205efa9ea7c063dfdd322f11fb44070571
Michael Danilov <michael@danilov.me>
2019-02-21 10:30:46 +0300

    Update job and city

--- src/data/index.ts
+++ src/data/index.ts
@@ -11,15 +11,15 @@ export default {
     en: [
       [
         '<a href="https://jamstack.org/" hreflang="en-US">JAM-stack</a> developer.',
-        'Worked at <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a>, <a href="https://livetyping.com/en/" hreflang="en-US">Live Typing</a>. Temporarily unemployed.',
-        'Living in <a href="https://en.wikipedia.org/wiki/Omsk" hreflang="en-US">Omsk, Russia</a>.',
+        'Worked at <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a>, <a href="https://livetyping.com/en/" hreflang="en-US">Live Typing</a>. Now working at <abbr title="NDA">██</abbr> company.',
+        'Lived in <a href="https://en.wikipedia.org/wiki/Omsk" hreflang="en-US">Omsk, Russia</a>. Now living in <a href="https://en.wikipedia.org/wiki/Moscow" hreflang="ru-RU">Moscow, Russia</a>',
       ].join("<br>"),
     ],
     ru: [
       [
         '<a href="https://jamstack.org/" hreflang="en-US">JAM-stack</a> разработчик.',
-        'Работал в <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a>, <a href="https://livetyping.com/ru/" hreflang="ru-RU">Лайв Тайпинге</a>. Временно безработный.',
-        'Живу в <a href="https://ru.wikipedia.org/wiki/Омск" hreflang="ru-RU">Омске</a>.',
+        'Работал в <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a>, <a href="https://livetyping.com/ru/" hreflang="ru-RU">Лайв Тайпинге</a>. Сейчас работаю в компании <abbr title="NDA">██</abbr>.',
+        'Жил в <a href="https://ru.wikipedia.org/wiki/Омск" hreflang="ru-RU">Омске</a>. Сейчас в <a href="https://ru.wikipedia.org/wiki/Москва" hreflang="ru-RU">Москве</a>.',
       ].join("<br>"),
     ],
   },
