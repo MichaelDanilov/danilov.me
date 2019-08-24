561d5377daeed64142aa606cab33150d2cae057e
Michael Danilov <michael@danilov.me>
2019-08-24 15:03:16 +0300

    add jest and infrastructure for tests

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?)$',
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.js',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  testPathIgnorePatterns: ['node_modules', '.cache'],
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+  globals: {
+    __PATH_PREFIX__: '',
+  },
+  testURL: 'http://localhost',
+  setupFiles: ['<rootDir>/loadershim.js'],
+};
