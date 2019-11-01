ce886e8d4e45faf6aac9faf801fa12919ceaf9b2
Michael Danilov <michael@danilov.me>
2019-11-01 07:57:11 +0300

    Update own position on index page

--- src/data/index.ts
+++ src/data/index.ts
@@ -9,14 +9,16 @@ export default {
     en: [
       [
         '<a href="https://jamstack.org/" hreflang="en-US">JAM-stack</a> developer.',
-        'Worked at <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a>, <a href="https://livetyping.com/en/" hreflang="en-US">Live Typing</a>. Now working at <abbr title="NDA">██</abbr> company.',
+        'Worked at <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a>, <a href="https://livetyping.com/en/" hreflang="en-US">Live Typing</a>.',
+        'Now working at <a href="https://yandex.com/" hreflang="en-US">Yandex</a> in <a href="https://www.kinopoisk.ru/" hreflang="en-US">KinoPoisk</a> front-end team.',
         'Lived in <a href="https://en.wikipedia.org/wiki/Omsk" hreflang="en-US">Omsk, Russia</a>. Now living in <a href="https://en.wikipedia.org/wiki/Moscow" hreflang="ru-RU">Moscow, Russia</a>',
       ].join('<br>'),
     ],
     ru: [
       [
         '<a href="https://jamstack.org/" hreflang="en-US">JAM-stack</a> разработчик.',
-        'Работал в <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a>, <a href="https://livetyping.com/ru/" hreflang="ru-RU">Лайв Тайпинге</a>. Сейчас работаю в компании <abbr title="NDA">██</abbr>.',
+        'Работал в <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a>, <a href="https://livetyping.com/ru/" hreflang="ru-RU">Лайв Тайпинге</a>.',
+        'Сейчас работаю в компании <a href="https://yandex.ru/" hreflang="ru-RU">Яндекс</a> в команде фронтенд-разработки <a href="https://www.kinopoisk.ru/" hreflang="ru-RU">КиноПоиска</a>.',
         'Жил в <a href="https://ru.wikipedia.org/wiki/Омск" hreflang="ru-RU">Омске</a>. Сейчас в <a href="https://ru.wikipedia.org/wiki/Москва" hreflang="ru-RU">Москве</a>.',
       ].join('<br>'),
     ],
