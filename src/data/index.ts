24c3b300b93feb3b6af3874a98668ab5fb3839e4
Michael Danilov <michael@danilov.me>
2020-02-09 10:22:24 +0600

    Update texts (#7)
    * update main page texts
    
    * moikrug => career.habr
    
    * update tests

--- src/data/index.ts
+++ src/data/index.ts
@@ -9,15 +9,15 @@ export default {
     en: [
       [
         '<a href="https://jamstack.org/" hreflang="en-US">JAM-stack</a> developer.',
-        'Worked at <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a>, <a href="https://livetyping.com/en/" hreflang="en-US">Live Typing</a>.',
-        'Now working at <a href="https://yandex.com/" hreflang="en-US">Yandex</a> in <a href="https://www.kinopoisk.ru/" hreflang="en-US">KinoPoisk</a> front-end team.',
-        'Lived in <a href="https://en.wikipedia.org/wiki/Omsk" hreflang="en-US">Omsk, Russia</a>. Now living in <a href="https://en.wikipedia.org/wiki/Moscow" hreflang="ru-RU">Moscow, Russia</a>',
+        'Used to work in <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a> and <a href="https://livetyping.com/en/" hreflang="en-US">Live Typing</a>.',
+        'Now working in <a href="https://yandex.com/" hreflang="en-US">Yandex</a> in <a href="https://www.kinopoisk.ru/" hreflang="en-US">KinoPoisk</a> Frontend Development group.',
+        'Used to live in <a href="https://en.wikipedia.org/wiki/Omsk" hreflang="en-US">Omsk, Russia</a>. Now living in <a href="https://en.wikipedia.org/wiki/Moscow" hreflang="ru-RU">Moscow, Russia</a>',
       ].join('<br>'),
     ],
     ru: [
       [
         '<a href="https://jamstack.org/" hreflang="en-US">JAM-stack</a> разработчик.',
-        'Работал в <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a>, <a href="https://livetyping.com/ru/" hreflang="ru-RU">Лайв Тайпинге</a>.',
+        'Работал в <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a> и <a href="https://livetyping.com/ru/" hreflang="ru-RU">Лайв Тайпинге</a>.',
         'Сейчас работаю в компании <a href="https://yandex.ru/" hreflang="ru-RU">Яндекс</a> в команде фронтенд-разработки <a href="https://www.kinopoisk.ru/" hreflang="ru-RU">КиноПоиска</a>.',
         'Жил в <a href="https://ru.wikipedia.org/wiki/Омск" hreflang="ru-RU">Омске</a>. Сейчас в <a href="https://ru.wikipedia.org/wiki/Москва" hreflang="ru-RU">Москве</a>.',
       ].join('<br>'),
