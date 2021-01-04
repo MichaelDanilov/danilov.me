4c4fa6bcf8991faa8602b59ab33224ffeeff7962
Michael Danilov <michael@danilov.me>
2021-01-04 15:12:31 +0300

    JAM-stack => Jamstack (#13)

--- src/data/index.ts
+++ src/data/index.ts
@@ -8,7 +8,7 @@ export default {
   content: {
     en: [
       [
-        '<a href="https://jamstack.org/" hreflang="en-US">JAM-stack</a> developer.',
+        '<a href="https://jamstack.org/" hreflang="en-US">Jamstack</a> developer.',
         'Used to work in <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a> and <a href="https://livetyping.com/en/" hreflang="en-US">Live Typing</a>.',
         'Now working in <a href="https://yandex.com/" hreflang="en-US">Yandex</a> in <a href="https://www.kinopoisk.ru/" hreflang="en-US">KinoPoisk</a> Frontend Development group.',
         'Used to live in <a href="https://en.wikipedia.org/wiki/Omsk" hreflang="en-US">Omsk, Russia</a>. Now living in <a href="https://en.wikipedia.org/wiki/Moscow" hreflang="ru-RU">Moscow, Russia</a>',
@@ -16,7 +16,7 @@ export default {
     ],
     ru: [
       [
-        '<a href="https://jamstack.org/" hreflang="en-US">JAM-stack</a> разработчик.',
+        '<a href="https://jamstack.org/" hreflang="en-US">Jamstack</a> разработчик.',
         'Работал в <a href="https://ningen.group/" hreflang="en-US">Ningen Group</a> и <a href="https://livetyping.com/ru/" hreflang="ru-RU">Лайв Тайпинге</a>.',
         'Сейчас работаю в компании <a href="https://yandex.ru/" hreflang="ru-RU">Яндекс</a> в команде фронтенд-разработки <a href="https://www.kinopoisk.ru/" hreflang="ru-RU">КиноПоиска</a>.',
         'Жил в <a href="https://ru.wikipedia.org/wiki/Омск" hreflang="ru-RU">Омске</a>. Сейчас в <a href="https://ru.wikipedia.org/wiki/Москва" hreflang="ru-RU">Москве</a>.',
